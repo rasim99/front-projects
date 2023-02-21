3e5c16da44c0f8d9986b4a38e7cb5b907d8c0b1e
rasim99 <rasimjm@code.edu.az>
2023-02-21 14:13:53 +0400

    Array methods (push,shift,unshift,pop)

---
+++ assets/JS-array methods/assets/script.js
@@ -0,0 +1,8 @@
+let arr=[];
+arr.push("array-string") //add element to array
+arr.push(["array-string-in-array"])
+arr.unshift("first") //add element to array like first element
+arr.pop();   //delete end element from array
+arr.push("end")
+arr.shift(); //delete first element  from array
+console.log(arr);
