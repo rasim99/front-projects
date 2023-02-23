ecedeb6701d8e4de887270615a5aae03c12d1755
rasim99 <rasimjm@code.edu.az>
2023-02-23 10:37:03 +0400

    class caculator

---
+++ assets/JS_Caculator/script.js
@@ -0,0 +1,11 @@
+class cacuclator{
+    constructor(num){
+        this.num=Number(parseInt(prompt("ededyaaaaz")))
+    }
+    sum(num){
+        this.num+=num
+        return this
+    }
+}
+let dd=new cacuclator(num).sum(11)
+console.log(dd);
