ab7c993d2d157c69a9ab8138f4b5332abd9356c6
rasim99 <rasimjm@code.edu.az>
2023-03-04 12:21:47 +0400

    onclick

---
+++ js onclick/script.js
@@ -0,0 +1,14 @@
+let OnclickDiv1=document.getElementById("div1");
+let OnclickDiv2=document.getElementById("div2");
+let OnclickDiv3=document.getElementById("div3");
+OnclickDiv1.onclick=function() {
+   OnclickDiv1.style.visibility="hidden"
+}
+OnclickDiv2.onclick=function() {
+    OnclickDiv2.style.visibility="hidden"
+ }
+ 
+ OnclickDiv3.onclick=function() {
+    OnclickDiv3.style.visibility="hidden"
+ }
+ 
